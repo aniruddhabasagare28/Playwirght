c5175a5605960130cd48d7e557b892e37297a717
Aniruddha <aniruddhabasagare28@gmail.com>
2026-09-05 15:10:07 +0530

    consitional statement

---
+++ TypeScript-Basics-Advance/conditionalStatement.ts
@@ -0,0 +1,36 @@
+let studentAge: number = 20;
+if (studentAge >= 18) {
+    console.log("Student is an adult.");    
+} else {
+    console.log("Student is a minor.");
+}
+
+let day =1;
+
+switch(day){
+    case 1:
+        console.log("Today is Monday.");
+        break;
+    case 2:
+        console.log("Today is Tuesday.");
+        break;
+    case 3:
+        console.log("Today is Wednesday.");
+        break;
+    case 4:
+        console.log("Today is Thursday.");
+        break;
+    case 5:
+        console.log("Today is Friday.");
+        break;
+    case 6:
+        console.log("Today is Saturday.");
+        break;
+    case 7:
+        console.log("Today is Sunday.");
+        break;
+    default:
+        console.log("Invalid day.");
+}
+
+
